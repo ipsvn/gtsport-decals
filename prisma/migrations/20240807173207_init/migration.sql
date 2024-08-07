6334cfc3f1eca4898d7c6d217f8ee21754e0638d
svn <ipsvn@protonmail.com>
2024-08-07 21:04:29 +0300

    reinit db, seeding working

--- prisma/migrations/20240807173207_init/migration.sql
+++ prisma/migrations/20240807173207_init/migration.sql
@@ -1,26 +1,26 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" BIGINT NOT NULL PRIMARY KEY,
     "name" TEXT,
     "country" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "Decal" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" BIGINT NOT NULL PRIMARY KEY,
     "create_time" DATETIME NOT NULL,
     "title" TEXT NOT NULL,
     "comment" TEXT NOT NULL,
     "status" INTEGER NOT NULL,
     "open" INTEGER NOT NULL,
     "keyword" TEXT NOT NULL,
-    "userId" INTEGER NOT NULL,
+    "userId" BIGINT NOT NULL,
     CONSTRAINT "Decal_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "Tag" (
-    "decalId" INTEGER NOT NULL,
+    "decalId" BIGINT NOT NULL,
     "tag" TEXT NOT NULL,
 
     PRIMARY KEY ("decalId", "tag"),
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,19 +8,59 @@ async function main() {
 
     for await (const match of glob('data/**/*.json')) {
 
+        console.log(`parse ${match}`)
         const file = await readFile(match, { encoding: 'utf8' });
+        
+        let json: any = {};
+        try {
+          json = JSON.parse(file);
+        } catch (exc: any) {
+          console.error(`error parsing ${match}`);
+          continue;
+        }
 
-        console.log(file);
+        const userNo  = BigInt(json.user_no);
+        const decalId = BigInt(json.decal_id);
+
+        const user = await prisma.user.upsert({
+          create: {
+            id: userNo,
+            name: json.user_id,
+            country: json.country,
+          },
+          update: {
+            name: json.user_id,
+            country: json.country,
+          },
+          where: {
+            id: userNo
+          }
+        });
+
+        const tags: string[] = json.tags;
+
+        await prisma.decal.create({
+          data: {
+            id: decalId,
+            create_time: new Date(json.create_time),
+            title: json.title,
+            comment: json.comment,
+            status: parseInt(json.status),
+            open: parseInt(json.open),
+            keyword: json.keyword,
+            tags: {
+              createMany: {
+                data: tags.map(it => ({tag: it}))
+              }
+            },
+            user: {
+              connect: user
+            }
+          }
+        });
 
     }
-    // glob('data/**/*.json', (err: any, matches: any) => {
 
-    //     for (let match of matches) {
-    //         console.log(match);
-    //     }
-
-    // });
-    
 }
 
 main()
