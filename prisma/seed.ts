1072ade279e116c00c47308b7d014160820db6fc
svn <ipsvn@protonmail.com>
2024-08-09 23:08:08 +0300

    make seeder use transaction, rather than the slow trash before

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,6 +7,9 @@ const prisma = new PrismaClient()
 
 async function main() {
 
+    let batch: any[] = [];
+    let batchNumber = 0;
+
     const dataGlob = new Glob('data/**/*.json', {});
     for await (const match of dataGlob) {
 
@@ -24,49 +27,51 @@ async function main() {
         const userNo  = BigInt(json.user_no);
         const decalId = BigInt(json.decal_id);
 
-        const user = await prisma.user.upsert({
-          create: {
-            id: userNo,
-            name: json.user_id,
-            country: json.country,
-          },
-          update: {
-            name: json.user_id,
-            country: json.country,
-          },
-          where: {
-            id: userNo
-          }
-        });
-
         const tags: string[] = json.tags;
 
-        await prisma.decal.upsert({
-          create: {
-            id: decalId,
-            create_time: new Date(json.create_time),
-            title: json.title,
-            comment: json.comment,
-            status: parseInt(json.status),
-            open: parseInt(json.open),
-            keyword: json.keyword,
-            tags: {
-              createMany: {
-                data: tags.map(it => ({tag: it}))
+        batch.push(
+          prisma.decal.upsert({
+            create: {
+              id: decalId,
+              create_time: new Date(json.create_time),
+              title: json.title,
+              comment: json.comment,
+              status: parseInt(json.status),
+              open: parseInt(json.open),
+              keyword: json.keyword,
+              tags: {
+                createMany: {
+                  data: tags.map(it => ({tag: it}))
+                }
+              },
+              user: {
+                connectOrCreate: {
+                  create: {
+                    id: userNo,
+                    name: json.user_id,
+                    country: json.country
+                  },
+                  where: {
+                    id: userNo
+                  }
+                }
               }
             },
-            user: {
-              connect: user
+            update: {},
+            where: {
+              id: decalId
             }
-          },
-          update: {
-            
-          },
-          where: {
-            id: decalId
-          }
-        });
+          })
+        );
 
+        if (batch.length > 10000)
+        {
+          batchNumber++;
+          console.log("run batch " + batchNumber);
+          await prisma.$transaction(batch);
+          batch = [];
+        }
+        
     }
 
 }
