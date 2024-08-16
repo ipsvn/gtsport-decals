a0e825d8f3025c0ed75ac53f1480570b489187a1
Dehy <dehyxd@gmail.com>
2024-08-17 01:04:57 +0200

    did like something i guess

--- src/components/DecalModal.tsx
+++ src/components/DecalModal.tsx
@@ -8,6 +8,7 @@ import Box from "@mui/material/Box";
 import { getDecalImageUrl } from "@/utils/utils";
 import Image from "next/image";
 import { Button } from "@mui/material";
+import downloadIcon from "/public/download.svg";
 
 export interface DecalModalProps {
     open: boolean,
@@ -35,28 +36,32 @@ export function DecalModal(
             aria-describedby="decal-modal-description"
         >
             <Box
-                className="w-full max-w-[84rem] mx-auto outline-none absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-full max-h-[48rem]"
+                className="container-modal outline-none absolute top-1/2 -translate-y-1/2 h-full max-h-[48rem]"
             >
 
-                <div className="bg-dark-gray lg:grid grid-cols-4 gap-4 h-full">
-                    <div className="col-span-3 bg-slate-50 h-full">
-                        <div className="flex items-center justify-center w-full h-full bg-light-gray">
-                            <Image
-                                src={svgUrl}
-                                width={0}
-                                height={0}
-                                unoptimized={true}
-                                className="w-full h-auto object-contain aspect-3/2"
-                                style={{ objectFit: 'contain' }}
-                                sizes="100vw"
-                                alt="xd"
-                            />
-                        </div>
-
+                <div className="bg-dark-gray flex flex-col h-full">
+                    <div className="flex items-center justify-center w-full h-min bg-light-gray">
+                        <Image src={svgUrl}
+                            alt={decal.title}
+                            width={600}
+                            height={400}
+                            unoptimized={true}
+                            style={{ objectFit: 'contain' }}
+                            className="aspect-3/2"
+                        />
                     </div>
-                    <div className="col-span-1">
-                        {decal.title}
-                        <Button href={svgUrl} className="size-5" target="_blank" download={`${decal.title} - ${idString}`}>I like penis</Button>
+
+                    <div className="flex flex-col w-full p-4 lg:p-8">
+                        <h3 className="text-white text-3xl font-bold word-break">
+                            {decal.title}
+                        </h3>
+                        <p className="text">By: {decal.user.name}</p>
+                        <a href={svgUrl} className="size-5 flex-shrink-0" target="_blank" download={`${decal.title} - ${idString}`}>
+                            <Image
+                                src={downloadIcon}
+                                alt="Download icon"
+                            />
+                        </a>
                     </div>
                 </div>
 
